b6b614fc586ba3c0e4cfa5790c4a82318a1e62f0
kashsaeed <kashifnizami@gmail.com>
2021-07-21 17:47:24 +0200

    18. using Crud repositories

--- src/main/java/com/jrp/pma/dao/ProjectRepository.java
+++ src/main/java/com/jrp/pma/dao/ProjectRepository.java
@@ -3,6 +3,9 @@ package com.jrp.pma.dao;
 import com.jrp.pma.entities.Project;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+import java.util.List;
 
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    public List<Project> findAll();
 }
